dc514c0761bd4d93be98dfeff2b06f4beaa536b8
TrueC-D <aletheacary@gmail.com>
2021-06-02 22:16:22 -0700

    added the new need selects to classes in temp.js, copied from need_selection_classes_not_used.js

--- front-end/src/temp.js
+++ front-end/src/temp.js
@@ -38,14 +38,88 @@ class Pet {
     }
 
     init(pet_id, needSelections){
-        // this creates the default needSelections that every pet should have
+        // this instantiates the default needSelections that every pet should have by calling addNeedSelect on every item
         for(const needSelect of needSelections){
             this.addNeedSelect(pet_id, needSelect)
         }
     }
 
     addNeedSelect(pet_id, needSelect){
-        // this calls the function that has the fetch request for the needselect
+        // this calls the function that has the fetch request for the needselect.  This can be used for adding individual needs later.
         createNeedSelection(pet_id, needSelect.title, needSelect.description)
+    }
+}
+
+class AquaticSpecies extends Pet {
+    needSelect(){
+        let newNeedSelects = [  
+            {title: 'Clean Tank', description: "Changing the water in the tank and removing excess waste and algae will help marine life thrive."},
+            {title: 'Check chemical levels in tank.', description: "Tank water with the wrong pH levels, or water that is too high in high in nitrates, nitrites and ammonium can harm aquatic animals."},
+            {title: 'Turn On Light', description: "Animals in terrariums and aquariums also need a day and night cycle. Turn the light on to start their morning."},
+            {title: 'Turn Off Light', description: "Animals in terrariums and aquariums also need a day and night cycle. Turn the light off to start their evening."}
+        ]
+        const allNeedSelects = super.needSelects().concat(newNeedSelects)
+
+        return allNeedSelects
+    }
+
+    // init, create, and addNeedSelect methods shouldbe included by extension
+}
+
+class LandAnimal extends Pet {
+    needSelects(){
+        let newNeedSelects = [
+            {title: 'Give Water', description: "Pets that don't live in water need to drink water to stay hydrated."}
+        ]
+        const allNeedSelects = super.needSelects().concat(newNeedSelects)
+
+        return allNeedSelects
+    }
+}
+
+class PetWithACoat extends LandAnimal {
+    needSelects(){
+        let newNeedSelects = [
+            {title: 'Groom', description: "Grooming prevents matts from developing, prevents rashes and infection."}
+        ]
+        const allNeedSelects = super.needSelects().concat(newNeedSelects)
+
+        return allNeedSelects
+    }
+}
+
+class Cat extends PetWithACoat {
+    constructor(name, birthday, userId){
+        super(name)
+        super(birthday)
+        super(user_id)
+        this.species = 'Cat'
+    }
+
+    needSelects(){
+        let newNeedSelects = [
+            {title: 'Clean Litter Box', description: "A clean litter box helps prevent a messy house and sanitary living environment."}
+        ]
+        const allNeedSelects = super.needSelects().concat(newNeedSelects)
+
+        return allNeedSelects
+    }
+}
+
+class Dog extends PetWithACoat {
+    constructor(name, birthday, userId){
+        super(name)
+        super(birthday)
+        super(user_id)
+        this.species = 'Dog'
+    }
+
+    needSelects(){
+        let newNeedSelects = [
+            {title: 'Go On Walk', description: "Excercise is a must half for a healthy animal."}
+        ]
+        const allNeedSelects = super.needSelects().concat(newNeedSelects)
+
+        return allNeedSelects
     }
 }
