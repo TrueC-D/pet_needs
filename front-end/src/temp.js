8441eb8c91bc7bcd299fa07346950086902fb714
TrueC-D <aletheacary@gmail.com>
2021-06-03 00:27:25 -0700

    added link to json/class conversion? but i could probably do without it

--- front-end/src/temp.js
+++ front-end/src/temp.js
@@ -1,1 +1,21 @@
-git
+function loadUserPage(){
+    // should convert remove hidden class from create pet form
+    // should call method to create pet cards
+    // should 
+}
+
+function getPets(){
+    // https://stackoverflow.com/questions/37928113/mapping-json-to-es6-classes
+    // the above link shows how to parse jason to get values for new Pet
+    // fetch and... create pet javascript object?
+    // pet = new Pet for each
+    // store need selections?  if already created, do not need to initialize need selections but do need to fetch need selections
+    // 
+}
+
+function createPetCard(){
+    // should be used with fetch for each pet found and for any new peets
+    // delete pet button
+    // delete li button should be usable 
+    // create li buttion? 
+}
