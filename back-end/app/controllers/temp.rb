12d83da8205c24715befaa67393d67ac0931fd90
TrueC-D <aletheacary@gmail.com>
2021-06-02 19:01:52 -0700

    re made users controller and everything works again? not sure why there was a glitch before

--- back-end/app/controllers/temp.rb
+++ back-end/app/controllers/temp.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
     def index
         users = User.all
         render json: UserSerializer.new(users)
@@ -9,24 +8,28 @@ class UsersController < ApplicationController
         user = User.find_by(id: params[:id])
         options = {include: [:pets]}
         render json: UserSerializer.new(user, options)
-        # should show all pets and needs under each pet
-        # needs should be ordered by soonest (stretch goal)
     end
 
     def create
-        if User.find_by(name: params[:name])
+        if User.find_by(name: 'params[:name]')
             'username is already taken'
         elsif params[:name].strip == ''
-           'must input username before submitting'
+            'must input username before submitting'
         else
             user = User.create(name: params[:name])
             render json: UserSerializer.new(user)
-        end
+        end       
     end
 
     def destroy
-        binding.pry
-         # stretch goal: edit this so that it will remove all the pets and needs as well when the user is deleted (don't want to clutter database)
+       
         render json: User.find(params[:id]).destroy
     end
+    # user is initialized with a name
+    # index page to select a user
+    # on the index page you should be able to create a new user
+    # show page to see user's pets & needs
+    # should be able to delete the user via the show page which will redirect to the index ()
+    # on the show you should be able to add a pet and remove a pet, creating the name and species
 end
+
