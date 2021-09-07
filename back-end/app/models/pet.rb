25b3f39d77041d393b5587227ad9ba9d8d0f62c3
TrueC-D <aletheacary@gmail.com>
2021-09-06 22:18:56 -0700

    need selections can be created in pet model? - doesn't follow mvc

--- back-end/app/models/pet.rb
+++ back-end/app/models/pet.rb
@@ -3,5 +3,7 @@ class Pet < ApplicationRecord
   has_many :needs, dependent: :destroy
   has_many :need_selections, dependent: :destroy
   # need selections are a separate class which allows you to have a saved type of need so to auto recreate a regular need instance while typing less info
-  
+  # def dog do 
+    # creates dog & need selections
+  # end  
 end
