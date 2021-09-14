e605e9288619afbbe570f0a71774702ec2d425fc
TrueC-D <aletheacary@gmail.com>
2021-09-13 22:43:32 -0700

    ruby case statement

--- back-end/app/controllers/pets_controller.rb
+++ back-end/app/controllers/pets_controller.rb
@@ -17,6 +17,31 @@ class PetsController < ApplicationController
         # need to change controller to receive params differently.  if species is within labeled type of species, appropriate need selections need to be created w/validations.
         user = User.find_by(id: params[:user_id])
         if user && !(Pet.find_by(name: params[:name]))
+            # need to create params[:type]
+            # if params[:type] == 
+            species = params[:species].capitalize
+            # may need to validate species
+            case params[:type]
+            when "Dog"
+                create_pet("Dog")
+                # method for needselects dog
+            when "Cat"
+                create_pet("Cat")
+                # method for needselects cat
+            when "AquaticSpecies"
+                create_pet(species)
+                # method for needselects aqua speicies
+            when "LandAnimal"
+                create_pet(species)
+                # method for needselects land animal
+            when "PetWithCoat"
+                create_pet(species)
+            else 
+                # may need to create other category -> else can be for validation purposes -reject item
+                create_pet(params[:species].captialize)
+            end
+
+
 
             # pet = Pet.create(name: params[:name], species: params[:species], birthday: params[:birthday], user_id: params[:user_id])
             # render json: PetSerializer.new(pet)
@@ -25,5 +50,12 @@ class PetsController < ApplicationController
 
     def destroy
         render json: Pet.find_by(id: params[:id]).destroy
+    end
+
+    private 
+
+    def create_pet(species)
+        
+
     end
 end
